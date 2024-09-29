25f0c46ef82fd7b9263e3c0e7ddb292d0411dff7
RedwanShahriarShubho_17 <142111295+redwanshahriarshubho@users.noreply.github.com>
2024-09-29 13:02:30 +0600

    fast api code

---
+++ main.py
@@ -0,0 +1,29 @@
+import os
+
+import dotenv
+import fastapi
+
+from back_end.server.api_model import PromptInput
+from back_end.llm.search_model import Chain
+
+dotenv.load_dotenv()
+
+model_id = 'microsoft/Phi-3-mini-128k-instruct'
+api_key = {
+    'tavily': os.getenv('TAVILY_API_KEY'),
+    'hf_k': os.getenv('HF_API_KEY'),
+    'wv_k': os.getenv('WEAVIATE_API_KEY'),
+    'wv_url': os.getenv('WEAVIATE_URL')
+}
+
+rag_chain = Chain(model_id=model_id, api_key=api_key)
+
+app = fastapi.FastAPI()
+
+@app.get('/')
+def home():
+    return "hello world"
+
+@app.post('/prompt')
+def prompt_llm(prompt: PromptInput):
+    return rag_chain.prompt_chain(prompt.input)
